ab32648d6e7411a9fc51c0f717f757907f673362
yaried tekie <yaredsonj@gmail.com>
2018-06-12 11:10:44 -0500

    two way binidng

--- Lab11/Lab11/src/app/app.module.ts
+++ Lab11/Lab11/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter.component';
@@ -10,7 +11,8 @@ import { CounterComponent } from './counter.component';
     CounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Lab11/Lab11/src/app/counter.component.ts
+++ Lab11/Lab11/src/app/counter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
